e1f351acb10b9ac3e380b2adb266bd203c746faa
unknown <scotteychaos@gmail.com>
2022-04-25 19:07:29 -0400

    added styling

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
+let header = document.querySelector('#page-header')
+header.style.textAlign = "left"
 
-// Select some elements...
+
+let dogImages = document.querySelectorAll('.dog-image')
+for(let i = 0; i < dogImages.length; i++){
+	dogImages[i].style.borderRadius = '50px'
+}
+
+let dogNames = document.querySelectorAll('.dog-name')
+for(let i = 0; i < dogNames.length; i++){
+	dogNames[i].style.textAlign = "left"
+}
+
+let dogImage = document.querySelectorAll('.dog-image')
+for(let x = 0; x < dogImages.length; x++){
+	dogImages[x].style.transform = "rotate(180deg)"
+}
